7b577abef20d89d103b3562fd2792ee2daa117a9
WanerValencia2019 <valenciawaner@gmail.com>
2021-08-02 18:53:19 -0500

    update model student

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.db.models.signals import post_save
+from apps.courses.models import Course
+from django.dispatch import receiver
 
 User = get_user_model()
 
 # Create your models here.
-class PropertyUser(models.Model):
+class PropertyUser(models.Model):    
     FEMALE='FM'
     MALE='MA'
     GENDER = [
@@ -32,18 +36,23 @@ class PropertyUser(models.Model):
 
 
 class Student(PropertyUser):
-    pass
+    course=models.ForeignKey(Course,on_delete=models.CASCADE,verbose_name="Curso",related_name="student_course", null=True, blank=True)
 
+    def __str__(self):
+        return "{}-{}".format(self.id,self.get_fullName())
+    
+    def get_fullName(self):
+        return "{} {}".format(self.first_name,self.last_name)
 
-
+@receiver(post_save, sender=Student)
 def create_profile_student(sender,instance,**kwargs):    
-    if instance.user.id is None:
+    if instance.user is None:
         first_name,last_name=instance.first_name,instance.last_name
         email=first_name.split(" ")[0][:3].lower()+last_name.split(" ")[0].lower()+"@sgaone.edu.co"
         username=first_name[:3].lower()+last_name.split(" ")[0].lower()
         password="SGAOPEN2021"
-        #print(dir(create_user))
-        #group=Group.objects.get(name="Estudiante")
+        
+        createGroup,group=Group.objects.get_or_create(name="ESTUDIANTES")
         create_user=User()        
         create_user.username=username
         create_user.first_name=first_name
@@ -51,7 +60,10 @@ def create_profile_student(sender,instance,**kwargs):
         create_user.email=email
         create_user.set_password(password)
         create_user.save()
-        #create_user.groups.set([group])        
+        if group is None:
+            create_user.groups.set([createGroup])  
+        else:
+            create_user.groups.set([group])  
         instance.user=create_user
         instance.save()    
     
